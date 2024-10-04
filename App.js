df8ce97886c3504b0035a51b36e44ac9185cdaf0
vektor8891 <38993315+vektor8891@users.noreply.github.com>
2024-10-04 13:14:23 -0400

    Refactor Square component import in App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Square } from "./src/components/Square";
+import { Square } from "./src/components/Square_click";
 import React from "react";
 
 export default function App() {
